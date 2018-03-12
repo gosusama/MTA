dc6bd1f1c7fed1af3337f3f76c3cd6a1cd7385d6
gosusama <33152868+gosusama@users.noreply.github.com>
2018-03-12 22:11:52 +0700

    ----hOÀNG ANH

---
+++ MTA.SERVICE/NV/DmGioiThieuService.cs
@@ -0,0 +1,23 @@
+﻿using MTA.ENTITY.NV;
+using MTA.SERVICE.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTA.SERVICE.NV
+{
+    public interface IDmGioiThieuService : IDataInfoService<Dm_GioiThieu>
+    {
+
+    }
+
+    public class DmGioiThieuService : DataInfoServiceBase<Dm_GioiThieu>, IDmGioiThieuService
+    {
+        public DmGioiThieuService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+    }
+}
